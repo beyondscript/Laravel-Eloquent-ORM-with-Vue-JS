3ccabc77a86c02d85dbbf026b1c59f5c6d7cf3ac
Beyondscript <nafiul1351@gmail.com>
2024-09-18 14:58:55 +0600

    5th commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="robots" content="noindex, nofollow">
         <meta property="og:image" content="{{ asset('images/icons/facebookimage.png') }}">
 
-        <link rel="icon" type="image/webp" href="{{asset('images/icons/favicon.webp')}}">
+        <link rel="icon" type="image/png" href="{{asset('images/icons/favicon.png')}}">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', function () {
-    return view('welcome');
-})->where('any','.*');
+Route::middleware(['www'])->group(function () {
+    Route::get('/{any}', function () {
+        return view('welcome');
+    })->where('any','.*');
+});
